567bc8ffdeea7ffe0abd46c523c1ff4fa60f8805
bluguja <bluguja@gmail.com>
2021-04-23 22:52:45 -0400

    Added route for post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :users
   post '/authenticate', to: 'authentication#authenticate'
   delete '/sign_out', to: 'authentication#sign_out'
