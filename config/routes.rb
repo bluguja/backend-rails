b6b6ae873ca993878ef3b70496cfd955276835b7
bluguja <bluguja@gmail.com>
2021-04-27 07:21:52 -0400

    route updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+  resources :comments
   resources :users
   post '/authenticate', to: 'authentication#authenticate'
   delete '/sign_out', to: 'authentication#sign_out'
