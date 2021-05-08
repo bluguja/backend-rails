fdc1325b6e1183721293b97d2d5cfe4bbe4442b2
Bala Luguja <bluguja@gmail.com>
2021-05-08 03:03:52 -0600

    updated .env file & added comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :comments
   resources :users
   post '/authenticate', to: 'authentication#authenticate'
-  delete '/sign_out', to: 'authentication#sign_out' # firts is controller second is method
+  delete '/sign_out', to: 'authentication#sign_out' # first is controller second is method
   get '/likes/:likable_type/:likable_id', to: 'likes#create'
 end
